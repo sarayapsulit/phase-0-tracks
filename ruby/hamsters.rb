79b7fdb82c2c0c4c76645db3e8bd9118e40a2ab8
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-05-11 11:28:31 -0400

    Process string input and Return feedback

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -6,21 +6,42 @@
 # 5.Age
 
 puts "What is the hamster's name?"
-name = gets.chomp
+name = gets.chomp.capitalize
 
 puts "On a scale from 1 to 10, 10 being the loudest, how loud do you think the hamster is?"
-volume = gets.chomp
+volume = gets.chomp.to_i
+
+if volume >= 7
+    volume = " very loud"
+elsif volume >= 4
+    volume = "quite noisy"
+else 
+    volume= "very quiet"
+end
 
 puts "What color is the fur?"
-fur= gets.chomp
+color= gets.chomp
 
 puts "Is the hamster a good candidate for adoption? (yes/no)"
-adoptability = gets.chomp
+    adopt = gets.chomp
+ if adopt =="yes" 
+    adopt = true
+    adopt = "ready"
+ else 
+    adopt = false
+    adopt = "not ready"
+    
+ end
+    
 
 puts "How old do you think the Hamster is?"
 age = gets.chomp
  if age.empty?
    age = nil
+     age = "undetermined"
  else 
    age = age.to_i 
   end
+
+puts "#The #{color} hamster named, #{name}, age #{age}, who can #{volume}, is #{adopt} for adoption."
+
