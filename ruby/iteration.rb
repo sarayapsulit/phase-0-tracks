e5517a9e200dae609c62ff92b16eec49f13782f5
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-05-16 11:20:19 -0400

    Add method and data structures

---
+++ ruby/iteration.rb
@@ -0,0 +1,18 @@
+#def exercise 
+#   print " what is your favorite color?"
+#color = gets.chomp
+#   yield (color)
+# end
+
+
+#exercise { |color| puts "your favorite color is  #{color}" }
+
+states =  ["New York", "California", "Illinois", "Texas"] 
+locations = {
+    "New York" => "New York City", 
+    "California" => "San Franscisco",
+    "Illinios" => "Chicago",
+    "Texas" => "Dallas"} 
+
+p states
+p locations
