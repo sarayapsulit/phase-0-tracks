47ce89a7bd74f48198dc2b6e02f7fa79ae748f7c
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-05-19 07:01:43 -0400

    Access nested items

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -3,7 +3,7 @@ pet_store = {
         cats: ["Siamese", "Persian", "Maine Coon"],
         dogs: ["Pug", "Beagle", "Corgi"],
         fish: ["Goldfish"," Angelfish","Guppy"]
-        }
+        },
         
     services: {
         grooming: ["Wash", "Hair Cut", "Brush"],
@@ -11,35 +11,51 @@ pet_store = {
             jim: {
                 wash: true, 
                 haircut: true, 
-                brush: false} 
+                brush: false}, 
             stacey: {
                 wash: true, 
                 haircut: true, 
-                brush:true} 
+                brush: true}, 
             tom: {
                 wash: true,
                 haircut: false, 
                 brush: true}}
-        }
-    products:{ 
-        food:{ 
-            dogs:{ 
+        },
+    products: { 
+        food: { 
+            dogs: { 
                 intial_inventory:100,
                 bags_left: 80 }, 
-            cats:{ 
+            cats: { 
                 intial_inventory:50,
                 bags_left: 45 },
-            fish:{ 
+            fish: { 
                 intial_inventory:30,
-                bags_left: 35 }}
+                bags_left: 35 }},
         homes: {
-            dogs:{
+            dogs: {
                 crates: ["wood", "wire", "plastic"],
-                beds: ["Small", "Medium", "Large"]}
-            cats:["bed","scratchpost","litterbox" ]
-            aquariums: {
-                freshwater: ["round", "square", "rectangle"],
-                saltwater:["round", "square", "rectangle"]}}
+                beds: ["Small", "Medium", "Large"]},
+            cats: ["bed","scratchpost","litterbox" ],
+            fish: {
+                aquariums: {
+                    freshwater: ["round", "square", "rectangle"],
+                    saltwater:["round", "square", "rectangle"]}}
         }
-    }
+    }}
+p pet_store[:animals]
+p pet_store[:services]
+p pet_store[:products]
+
+p pet_store[:products][:homes][:fish][:aquariums][:freshwater]
+p pet_store[:services][:stylists][:stacey]
+
+pet_store[:products][:homes][:cats][2].replace ("cat condo")
+p pet_store[:products][:homes][:cats]
+
+pet_store[:animals].delete(:fish)
+p pet_store[:animals]
+
+pet_store[:services][:medical]=["Vaccinations", "Deworming", "Testing"]
+p pet_store[:services]
 
