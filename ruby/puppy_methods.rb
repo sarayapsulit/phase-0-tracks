9b6efd804c0faddbee68e40c7a0c00be0818de14
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-05-25 10:23:48 -0400

    Add Speak Method to Puppy Class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,11 +1,18 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
-
+    def fetch(toy)
+        puts "I brought back the #{toy}!"
+        toy
+    end
+    
+    def speak(num)
+        num.times do puts "Woof!"
+        end
+    end
+    
 end
 
 jet = Puppy.new
 jet.fetch("bone")
+
+jet.speak(4)
