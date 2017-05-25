fba77b86e97ea6e911507df578f56a4b041852bd
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-05-25 09:44:48 -0400

    Create Puppy Class

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin
+Class: puppy
+
+Characteristics
+Size: Varies
+Color: Varies
+Fur: varies
+Nose: long 
+Ears: Pointed
+Tail: yes
+Friendly:yes 
+
+
+Behaviour
+
+Active
+Loyal
+Energetic
+=end
