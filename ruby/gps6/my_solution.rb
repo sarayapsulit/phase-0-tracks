bb1482e53ba9ed6008235aa324cfadde96030484
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-06-01 19:49:04 -0400

    Fix minor errors

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,6 +1,6 @@
 # Virus Predictor
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself ].
+# I spent [] hours on this challenge.
 # EXPLANATION OF require_relative
 
 
@@ -58,16 +58,16 @@ end
 #=======================================================================
 # DRIVER CODE
  # initialize VirusPredictor for each state
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"[:population_density], STATE_DATA["Alabama"][:population])
+alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
     
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"[:population_density], STATE_DATA["New Jersey"][:population])
+jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
 jersey.virus_effects
     
-california = VirusPredictor.new("California", STATE_DATA["California"[:population_density], STATE_DATA["California"][:population])
+california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
 california.virus_effects
     
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"[:population_density], STATE_DATA["Alaska"][:population])
+alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
     
 #=======================================================================
