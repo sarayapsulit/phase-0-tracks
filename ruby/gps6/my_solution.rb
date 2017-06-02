c2f3a76ad65d8854b22c81bf835970dcaa077657
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-06-02 10:07:37 -0400

    Answer Reflection

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -14,23 +14,26 @@ require_relative 'state_data'
 # Release 2: The Hash has all 50 states their population and their population density. The first level of Hash uses the state name as a string for the key and its value would the state data. the the second level would be the population and population as symbol keys to correspond to the statistics.  STATE_DATA is a constant variable not meant to be changed through out the program and constants being declared outside of the class makes it a global scope.
 
 class VirusPredictor
-    #Release 3: intializes the values
+    #Release 3: Intializes the values
     def initialize(state_of_origin, population_density, population)
         @state = state_of_origin
         @population = population
         @population_density = population_density
     end
-    # runs the two methods, predicted_deaths and the speed_of_spread
+    
+    # Release 3: Runs the two methods, predicted_deaths and the speed_of_spread
     def virus_effects
         predicted_deaths
         speed_of_spread
     end
     
-    private
     # Release 6: Private will hide all subsquent methods. If placed above 'virus_effects' it will hide and the drive code will no longer run. I would want to use this method when I only want to allow the user to use method I choose.
+    private
+  
     
     # Release 3: Calculates the number of deaths based on population.
     def predicted_deaths
+        
        # predicted deaths is solely based on population density
 #        if @population_density >= 200
 #            number_of_deaths = (@population * 0.4).floor
@@ -52,10 +55,13 @@ class VirusPredictor
         
         print "#{@state} will lose #{number_of_deaths} people in this outbreak"
      end
+        
     # Release 3: Defines the rate of spreading, giving an estimated time(months) for the disease to infect everyone in a given state depening on it's population density 
      def speed_of_spread(population_density, state) #in months
+         
         # We are still perfecting our formula here. The speed is also affected
         # by additional factors we haven't added into this functionality.
+         
 #        speed = 0.0
 #        if @population_density >= 200
 #            speed += 0.5
@@ -84,7 +90,7 @@ class VirusPredictor
          
          
          
- puts " and will spread across the state in #{speed} months.\n\n"
+         puts " and will spread across the state in #{speed} months.\n\n"
      end
 end
 #=======================================================================
@@ -92,14 +98,11 @@ end
  # initialize VirusPredictor for each state
     
 # Release 4: This doesn't belong in the class as its report for the user to see and the program makes this possible.
+    
 STATE_DATA.each do |state, data|
     infected_state = VirusPredictor.new(state, data[:population_density], data[:population])
     infected_state.virus_effects
 end
-
-
-
-
 
 #alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 #alabama.virus_effects
@@ -116,9 +119,21 @@ end
 #=======================================================================
 # Reflection Section
 
+# Release 8:
+# What are the differences between the two different hash syntaxes shown in the state_data file? 
+# The main difference between the two is the data type the key is in:  string and symbol. With he symbol type key it makes it easier for repeatative data to be readable.
 
+# What does require_relative do? How is it different from require?
+# Require Relative allows you to pull data from a different document unlike require which needs an absolute location.
 
+# What are some ways to iterate through a hash? 
+# You can use each or each_key each_value and map 
 
+# When refactoring virus_effects, what stood out to you about the variables, if anything? 
+# All the variables are instance variables and can be called upon with in the method rather than adding new information
+
+# What concept did you most solidify in this challenge? 
+# Definitely refactoring and making code simple and more DRY
 
 
 
