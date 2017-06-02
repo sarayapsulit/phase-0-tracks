c1192b942acbf530045ad3b5f6b6e91090c2763d
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-06-02 15:03:36 -0400

    Write reverse Function

--- js/explore.js
+++ js/explore.js
@@ -5,3 +5,13 @@
 //      then to the new first letter of the origString add the newString (pushing the letters reversely)
 //  do it until the word ends
 
+function reverse(str) {
+    var reversedWord = "";
+    
+    for (var i = 0; i < str.length; i ++){
+        reversedWord = str[i] + reversedWord
+    }
+    console.log(reversedWord)
+}
+         
+ reverse('hello')
