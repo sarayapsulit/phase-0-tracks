4c3a179e302974de6aa32e6299453bec5f1aeef8
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-06-02 15:01:14 -0400

    Write Psuedocode

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// Psuedocode for Reverse: 
+// Start with an empty newString()
+//      fill up the string with the first letter 
+//      remove letter from origString
+//      then to the new first letter of the origString add the newString (pushing the letters reversely)
+//  do it until the word ends
+
