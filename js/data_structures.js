bc73a73f72dc6f5e507e0944f2cfd086eebb6e08
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-06-02 15:56:29 -0400

    Add to the arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,5 @@
 var horseColors = [ "tan", "white", "black", "brown"] ;
-var horseNames = ["Glen", "Joey", "Mick", "Al"]
+var horseNames = ["Glen", "Joey", "Mick", "Al"];
+
+horseColors.push("Grey");
+horseNames.push("William");
