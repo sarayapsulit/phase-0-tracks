d19346fc84ebeacdc04ee81b0b70b158da8fae94
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-06-02 16:05:52 -0400

    Build Horse Object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,12 @@
 var horseColors = [ "tan", "white", "black", "brown"] ;
 var horseNames = ["Glen", "Joey", "Mick", "Al"];
 
-horseColors.push("Grey");
+horseColors.push("grey");
 horseNames.push("William");
+
+horse ={}
+for ( var i=0; i < horseNames.length; i++){
+    horse[horseNames[i]] = horseColors[i]
+};
+          console.log(horse)
+          
