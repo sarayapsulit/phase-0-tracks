2d86961b0d0414ab8fbc928379abed6c0b4412d0
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-06-03 15:23:46 -0400

    Make Car Constructor Function

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,5 +10,25 @@ for ( var i=0; i < horseNames.length; i++){
 };
           console.log(horse)
           
+var car = {fitsNumOfPeople: 6, color: 'black', isFast: true}
 
+function Car(fitsNumOfPeople, color, isFast) {
+    
+    console.log("This car:", this)
+    
+    this.fitsNumOfPeople = fitsNumOfPeople
+    this.color = color
+    this.isFast = isFast
+    
+    this.engine = function () {console.log("Vrooooom!");};
+    console.log("CAR INITIALIZATION COMPLETE");
+}
+
+console.log("Let's build a dog ...");
+
+var newCar = new Car(10, "white", false)
+console.log(newCar);
+console.log("When you turn on the car's engine it goes:");
+newCar.engine();
+console.log("-----");
 
