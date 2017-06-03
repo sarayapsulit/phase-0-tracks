9089266168791e8b5469d9c2e9fde7df12c6ef61
Sara Y. Sulit <sarayapsulit@gmail.com>
2017-06-02 23:46:55 -0400

    Refactor predicted deaths and speed of spread methods

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -35,60 +35,40 @@ class VirusPredictor
     def predicted_deaths
         
        # predicted deaths is solely based on population density
-#        if @population_density >= 200
-#            number_of_deaths = (@population * 0.4).floor
-#        elsif @population_density >= 150
-#            number_of_deaths = (@population * 0.3).floor
-#        elsif @population_density >= 100
-#            number_of_deaths = (@population * 0.2).floor
-#        elsif @population_density >= 50
-#            number_of_deaths = (@population * 0.1).floor
-#        else
-#            number_of_deaths = (@population * 0.05).floor
-#        end
-        
-        if 
-        
-        
-        
-        
+        if @population_density >= 200
+            multiplier = 0.4 
+        elsif @population_density >= 150
+            multiplier = 0.3
+        elsif @population_density >= 100
+            multiplier = 0.2
+        elsif @population_density >= 50
+            multiplier = 0.1
+        else
+            multiplier = 0.05
+        end
+    
+        number_of_deaths = (@population * multiplier).floor 
         
         print "#{@state} will lose #{number_of_deaths} people in this outbreak"
      end
         
     # Release 3: Defines the rate of spreading, giving an estimated time(months) for the disease to infect everyone in a given state depening on it's population density 
-     def speed_of_spread(population_density, state) #in months
+     def speed_of_spread #in months
          
         # We are still perfecting our formula here. The speed is also affected
         # by additional factors we haven't added into this functionality.
-         
-#        speed = 0.0
-#        if @population_density >= 200
-#            speed += 0.5
-#        elsif @population_density >= 150
-#            speed += 1
-#        elsif @population_density >= 100
-#            speed += 1.5
-#        elsif @population_density >= 50
-#            speed += 2
-#        else
-#            speed += 2.5
-#        end
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
+        
+         if @population_density >= 200
+             speed = 0.5
+         elsif @population_density >= 150
+             speed = 1
+         elsif @population_density >= 100
+             speed = 1.5
+         elsif @population_density >= 50
+             speed = 2
+         else
+             speed = 2.5
+         end
          
          puts " and will spread across the state in #{speed} months.\n\n"
      end
@@ -133,7 +113,7 @@ end
 # All the variables are instance variables and can be called upon with in the method rather than adding new information
 
 # What concept did you most solidify in this challenge? 
-# Definitely refactoring and making code simple and more DRY
+# Definitely refactoring and making code readable and more DRY
 
 
 
